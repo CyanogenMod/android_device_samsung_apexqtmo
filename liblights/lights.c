cee134f87473e2c24835efd854edbe8a8fe95df1
nardholio <nardholio@gmail.com>
2013-01-31 07:22:45 -0500

    keyboard backlight hack taken from maserati

--- liblights/lights.c
+++ liblights/lights.c
@@ -35,7 +35,7 @@ char const*const PANEL_FILE = "/sys/class/leds/lcd-backlight/brightness";
 char const*const BUTTON_FILE = "/sys/class/leds/button-backlight/brightness";
 
 char const*const LED_BLINK = "/sys/class/sec/led/led_blink";
-char const *const KEYBOARD_FILE = "/sys/class/leds/keyboard-backlight/brightness";
+char const*const KEYBOARD_FILE = "/sys/class/leds/keyboard-backlight/brightness";
 
 struct led_config {
     unsigned int color;
@@ -161,6 +161,8 @@ set_light_buttons(struct light_device_t* dev,
 
     pthread_mutex_lock(&g_lock);
     err = write_int(BUTTON_FILE, on?255:0);
+    //temporary hack until keyboard backlight support added to CM-10.1
+    err = write_int(KEYBOARD_FILE, on?255:0);
     pthread_mutex_unlock(&g_lock);
 
     return err;
