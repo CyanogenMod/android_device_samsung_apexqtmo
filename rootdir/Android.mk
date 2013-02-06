d3dd37a03184fe44f17cce33d492a2d5d159e7f4
David Daynard <nardholio@gmail.com>
2013-02-05 23:34:19 -0500

    Merge in Bluetooth from ville and comanche; other minor fixes
    Change-Id: I787690dc59a5f1d46220c60574105479222ca005

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -56,3 +56,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.wifi.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.bt.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.bt.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
