4a8dc024646c98a3f09969dde293cae011f5beb3
nardholio <nardholio@gmail.com>
2013-04-23 20:25:40 -0300

    Update build fingerprint with jellybean drop

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="apexqtmo-user 4.0.4 IMM76D T699UVALEM release-keys" PRIVATE_BUILD_DESC="samsung/apexqtmo/apexqtmo:4.0.4/IMM76D/T699UVALEM:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="samsung/apexqtmo/apexqtmo:4.1.2/JZO54K/T699UVBMC5:user/release-keys" PRIVATE_BUILD_DESC="apexqtmo-user 4.1.2 JZO54K T699UVBMC5 release-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-480x800
 
