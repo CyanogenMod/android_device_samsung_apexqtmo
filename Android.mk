c94e1ec745cf88183b5d5af17ffc3b5380f16cf6
David Daynard <nardholio@gmail.com>
2013-05-13 23:47:20 -0400

    Strengthen build barrier

--- Android.mk
+++ Android.mk
@@ -24,6 +24,8 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifneq ($(filter apexqtmo,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 endif
 endif
--- liblights/Android.mk
+++ liblights/Android.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter apexqtmo,$(TARGET_DEVICE)),)
-
 LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
@@ -32,4 +30,3 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif
