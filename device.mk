2ced00a52d72368c8cc8c58ee9b32c741c66ad2b
Shareef Ali <shareefalis@gmail.com>
2013-06-16 03:55:25 -0500

    fixup prop vendor

--- device.mk
+++ device.mk
@@ -14,12 +14,13 @@
 # limitations under the License.
 #
 
-## blobs
-$(call inherit-product-if-exists, vendor/samsung/apexqtmo/apexqtmo-vendor.mk)
+
 VARIENT_MODEL := apexqtmo
 VARIENT_REQUIRE_3.0_KERNEL := true
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/apexqtmo/apexqtmo-vendor.mk)
 ## common overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-gsm
 ## overlays
